e2e4017033d80cffadd8fbaec3deb786a93e2b49
nguyen duc <77447352+nguyenxuanduc789@users.noreply.github.com>
2024-04-02 01:29:16 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,1 +1,11 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+const concentDb = require('./config/db');
+const JokesRouter = require('./routes/Jokesroutes');
+concentDb();
+app.use(cors());
+app.use('/jokes/getallcontent', JokesRouter);
+app.listen(port, () => console.log(`App listening at http://192.168.164.1:${port}`));
 
